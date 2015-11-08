dcbc0622474daea88d55fa67e64bb13d416dbd71
atf1999 <torth212@gmail.com>
2015-11-07 21:33:50 -0500

    Ready for server

--- CrashBeacon/app/src/main/java/com/crash/beacon/MainActivity.java
+++ CrashBeacon/app/src/main/java/com/crash/beacon/MainActivity.java
@@ -1,29 +1,64 @@
 package com.crash.beacon;
 
+import android.content.Context;
 import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
     boolean isEnabled = false;
-    ImageButton btn;
-    TextView tv;
+    Button btn;
+    ListView lv;
+    String[] listItems = {"Home", "About", "Website"};
+    float lat = 0, lng = 0;
+    Sensor accel;
+    SensorManager manager;
+    float x = 0, y = 0, z = 0;
+    DrawerLayout dl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (ImageButton)findViewById(R.id.crash);
-        tv = (TextView)findViewById(R.id.state);
+        btn = (Button)findViewById(R.id.crash);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Crash Beacon");
         gui(isEnabled);
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accel = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Get the location manager
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, false);
+
+
+        Location location = locationManager.getLastKnownLocation(provider);;
+
+        // Initialize the location fields
+        if (location != null) {
+            onLocationChanged(location);
+        }
+
+        dl = (DrawerLayout)findViewById()
     }
 
     @Override
@@ -53,12 +88,65 @@ public class MainActivity extends AppCompatActivity {
     }
     public void gui(boolean state){
         if(state){
-            btn.setImageBitmap(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.crash));
-            tv.setText("Enabled");
+            btn.setText("Enabled");
         }
         else{
-            btn.setImageBitmap(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.crashg));
-            tv.setText("Disabled");
+            btn.setText("Disabled");
         }
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        manager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        manager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        x = event.values[0];
+        y = event.values[1];
+        z = event.values[2];
+        //Demo values
+        //For actual deployment, use lat and lng variables
+        float hLat = 39, hLng = -75;
+        if(isEnabled){
+            if(Math.abs(x) > 32 && Math.abs(y) > 25 && Math.abs(z) > 20){
+
+            }
+            else{
+
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = (float)location.getLatitude();
+        lng = (float)location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
